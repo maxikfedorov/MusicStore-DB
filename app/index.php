5f70b893612be6c6326e07eb048c706a9d923ef9
Maxim Fedorov <99714855+maxikfedorov@users.noreply.github.com>
2022-10-10 13:09:08 +0300

    DB App

--- app/index.php
+++ app/index.php
@@ -6,43 +6,32 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Тестовая страница</title>
     <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
 
 
 <body>
+
 <table>
-<?php
-$dbh = null;
-
-try {
-    $dbh = new PDO(
-        "mysql:host=mysql;dbname=test",
-        "ezh1k",
-        "alastor_cool",
-        [PDO::ATTR_PERSISTENT => true]
-    );
-
-    $stmt = $dbh->query("SELECT * FROM `test`.`User`");
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-    foreach ($rows as $row) {
-       /* echo "<div>{$row['user_id']}: {$row['surname']}</div>";*/
-        echo "<tr><td>{$row['user_id']}</td><td>{$row['name']}</td><td>{$row['surname']}</td></tr>";
-    }
-
-}
-
-catch (PDOException $e) {
-    echo "<div>{$e->getMessage()}</div>";
-    die();
-}
-?>
+    <?php include_once('output.php') ?>
 </table>
 
-<?php phpinfo(); ?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    Добавление элемента:
+    <input type="text" placeholder="ID" name="class_id" />
+
+    <input type="text" placeholder="Class" name="class_descr" />
+
+    <input type="submit" />
+</form>
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    Удаление элемента:
+    <input type="text" placeholder="ID" name="class_id_del" />
+
+    <input type="submit" />
+</form>
+
 </body>
 
 </html>
